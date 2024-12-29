0ad046792e30adea1da2adcea90ef5cad0431b23
Fat7allah <yassine.fat7allah@gmail.com>
2024-12-29 17:20:14 +0100

    fix yarn error

---
+++ fne/fne/public/js/fne.js
@@ -0,0 +1,15 @@
+frappe.provide('fne');
+
+fne = {
+    init: function() {
+        // Initialisation
+    },
+    
+    setup_filters: function() {
+        // Configuration des filtres
+    }
+};
+
+$(document).ready(function() {
+    fne.init();
+}); 
